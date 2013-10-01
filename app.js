95c783a2afa561a16ff17062380307936cad4e79
wbarksdale <wfbarksdale@gmail.com>
2013-10-01 11:53:22 -0700

    comments

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ $(function(){
 	var TodoVC = new MilkShake.ViewController($("#container"));
 	var todoList = new TodoList();
 
+	// Add the input item view
 	todoList.addSubview(new TodoInputItem(todoList));
 
+	// data
 	var todos = ["Take out the trash","Do Laundry","Feed the dog"];
 	var todoItems = [];
 
+	// create the todo item views
 	for(var i = 0; i < todos.length; i++)
 		todoItems.push(new TodoItem({todoText: todos[i]}));
 
+	// add the craeted views to the todoList
 	for(var i = 0; i < todoItems.length; i++){
 		todoList.addSubview(todoItems[i]);
 	}
 
+	// add the todolist as a subview to themain view controller
 	TodoVC.addSubview(todoList);
 });
