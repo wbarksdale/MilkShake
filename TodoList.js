267b314503bdcf120a4e830e3ec239fe952d1946
wbarksdale <wfbarksdale@gmail.com>
2013-10-01 11:03:08 -0700

    asdf

--- TodoList.js
+++ TodoList.js
@@ -1,9 +1,6 @@
 var DEBUGGER = false;	
 
 if(DEBUGGER) debugger;
-
-
-if(DEBUGGER) debugger;
 var TodoItem = MilkShake.View.extend({
 	init: function(vars){
 		this._super();
@@ -17,7 +14,14 @@ var TodoItem = MilkShake.View.extend({
 	},
 	template: '<li class="todo-item"><%= todoText %></li>',
 	css: {
-		
+		min_height: "25px",
+		line_height: "25px",
+		vertical_align: "middle",
+		padding: "10px",
+		background_color: "#FFFFFA",
+		border_style: "solid",
+		border_width: "0px 0px 1px 0px",
+		border_bottom_color: "#CCCCCC"
 	}
 });
 
@@ -26,7 +30,18 @@ var TodoList = MilkShake.View.extend({
 	init: function(){
 		this._super();
 	},
-	template: '<ul class="todo-list"></ul>'
+	template: '<ul class="todo-list"></ul>',
+	css: _.extend(
+		MilkShake.Styles.border_radius("5px"),
+		{
+			border_style: "solid",
+			border_width: "2px",
+			max_width: "400px",
+			margin:"auto",
+			padding:"0",
+			display:"block",
+			list_style_type: "none"
+		})
 });
 
 var TodoInputField = MilkShake.View.extend({
@@ -34,18 +49,19 @@ var TodoInputField = MilkShake.View.extend({
 		this._super();
 	},
 	getValue: function(){
-		if(this.view == undefined) 
-			throw "view is not defined";
-
+		if(this.view == undefined) throw "view is not defined";
 		return this.view.val();
 	},
 	clear: function(){
-		if(this.view == undefined) 
-			throw "view is not defined";
-
+		if(this.view == undefined) throw "view is not defined";
 		this.view.val('');
 	},
-	template: "<input type='text' placeholder='Whats there to do?'></input>"
+	template: "<input type='text' placeholder='Whats there to do?'></input>",
+	css: {
+			border_style: "none",
+			font_size:"25px",
+			min_width: "360px",
+	}
 });
 
 var TodoInputItem = MilkShake.View.extend({
@@ -64,5 +80,17 @@ var TodoInputItem = MilkShake.View.extend({
 			}.bind(this), "+")
 		);
 	},
-	template: '<li class="todo-input"></li>'
+	template: '<li"></li>',
+	css: _.extend({
+		display: "block",
+		min_height: "25px",
+		line_height: "25px",
+		vertical_align: "middle",
+		padding: "0px",
+		background_color: "#FFFFFA",
+		border_style: "solid",
+		border_width: "0px 0px 1px 0px",
+		border_bottom_color: "#CCCCCC",
+	},
+	MilkShake.Styles.inner_shadow("5px"))
 });
--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 
-var TodoVC = new MilkShake.ViewController($("#container"));
-var todoList = new TodoList();
+// Main app functionality
+$(function(){
+	var TodoVC = new MilkShake.ViewController($("#container"));
+	var todoList = new TodoList();
 
-todoList.addSubview(new TodoInputItem(todoList));
+	todoList.addSubview(new TodoInputItem(todoList));
 
-var todos = ["Take out the trash","Do Laundry","Feed the dog"];
-var todoItems = [];
+	var todos = ["Take out the trash","Do Laundry","Feed the dog"];
+	var todoItems = [];
 
-for(var i = 0; i < todos.length; i++)
-	todoItems.push(new TodoItem({todoText: todos[i]}));
+	for(var i = 0; i < todos.length; i++)
+		todoItems.push(new TodoItem({todoText: todos[i]}));
 
-for(var i = 0; i < todoItems.length; i++){
-	todoList.addSubview(todoItems[i]);
-}
+	for(var i = 0; i < todoItems.length; i++){
+		todoList.addSubview(todoItems[i]);
+	}
 
-TodoVC.addSubview(todoList);
-
-
+	TodoVC.addSubview(todoList);
+});
